8326711a931de331c7b90ddb0cbf26c34b32dcf6
kennedyannsouthern <kennedyannsouthern@gmail.com>
2025-06-29 22:34:00 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           Kennedy Southern
         </header>
 
-        {/* Hero Section */}
+        {/* Hero Layout */}
         <div className="h-screen flex flex-col justify-center items-center text-center space-y-6 px-4">
           <Image
             src="/kennedy-headshot.jpg"
@@ -30,25 +30,20 @@ export default function Home() {
             Data-Driven Solutions for a Better World
           </p>
 
-          {/* Social Links */}
+          {/* Social Media */}
           <div className="flex gap-6 pt-4 text-gray-400 text-xl">
-            <a href="mailto:kennedyannsouthern@gmail.com" className="hover:text-white transition">
-              Email
-            </a>
             <a
               href="https://www.linkedin.com/in/kennedy-southern-b2a8ab1a2/"
               target="_blank"
               rel="noopener noreferrer"
-              aria-label="LinkedIn"
               className="hover:text-white transition"
+              aria-label="LinkedIn"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="text-gray-400 hover:text-white transition"
+                className="h-6 w-6"
                 viewBox="0 0 24 24"
+                fill="currentColor"
               >
                 <path d="M4.98 3.5c0 1.38-1.12 2.5-2.49 2.5C1.12 6 0 4.88 0 3.5S1.12 1 2.49 1C3.86 1 4.98 2.12 4.98 3.5zM0 8h5v16H0V8zm7.5 0h4.7v2.2h.1c.7-1.3 2.3-2.2 4.1-2.2 4.1 0 4.6 2.7 4.6 6.2V24h-5v-8.6c0-2-.7-3.4-2.5-3.4-1.4 0-2.2.9-2.5 1.8-.1.3-.1.7-.1 1v9.2h-5V8z" />
               </svg>
