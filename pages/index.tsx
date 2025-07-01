3b510fdb79d60b53928fa1925be419c75591bab9
kennedyannsouthern <kennedyannsouthern@gmail.com>
2025-07-01 15:05:40 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             
             {/* Name */}
             <h1
-              className="text-5x1 font-bold text-pink-500 whitespace-nowrap">
+              className="text-3xl font-bold text-pink-500 whitespace-nowrap">
               Kennedy Southern
             </h1>
             
